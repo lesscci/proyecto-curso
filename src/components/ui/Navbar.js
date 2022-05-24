bafc9402a8958dae88f3baaf5f5d660da3237624
lesscci <102358696+lesscci@users.noreply.github.com>
2022-05-24 13:56:29 +0200

    imagen

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logocadscasdc.png";
+import logo from "../../assets/logo-blanco-recto.png";
 export const Navbar = () => {
   return (
       <nav className="Navbar">
